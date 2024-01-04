d4d7a24ef75339c117b45285117dfeed918d2bcb
engamir1 <engmohamed.amir101@gmail.com>
2024-01-05 01:57:38 +0200

    add create

--- task/urls.py
+++ task/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="home"),
+    path("add_task/", views.add_task, name="add_task"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here
+from .models import Task
 
 
 def home(request):
-    return render(request, "task/home.html")
+    tasks = Task.objects.all()
+    context = {"tasks": tasks}
+    return render(request, "task/home.html", context)
+
+
+def add_task(request):
+    task = request.POST["task"]
+    Task.objects.create(task=task)
+    return redirect("home")
